57b77cbd2ce33160270492152559501817773135
BeatyPete <akira941@gmail.com>
2020-11-13 04:30:07 -0600

    messing with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,53 @@ var movieFormEl = document.querySelector("#movie-form");
 var movieInputEl = document.querySelector("#movie-search-input");
 var resultsContainerEl = document.querySelector("#search-results-container");
 
-var displayMovies = function (movie) {
+var getOmdb = function (movieTitle) {
+  
+  var apiUrl =
+  `http://www.omdbapi.com/?s=${movieTitle}&apikey=65b2c758`;
+  // make a request to the url
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        // error message if an invalid entery/movie is submitted
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      // catch set up incase Open Weather is down or internet is disconnected
+      alert("Unable to connect to The Movie Database");
+    });
+};
+
+var getMovie = function () {
+  var apiUrl =
+    "https://api.themoviedb.org/3/find/tt1856101?api_key=b2b7dc79b0696d3f9c1db98685b5b36f&language=en-US&external_source=imdb_id"
+  // // make a request to the url
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was successful
+      if (response.true) {
+        response.json().then(function (data) {
+          console.log(data);
+          /* displaySearchMovies(data); */
+        });
+      } else {
+        // error message if an invalid entery/movie is submitted
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      // catch set up incase Open Weather is down or internet is disconnected
+      alert("Unable to connect to The Movie Database");
+    });
+};
+
+/* var displayMovies = function (movie) {
   console.log(movie);
   resultsContainerEl.innerHTML = "";
 
@@ -41,36 +87,7 @@ var displayMovies = function (movie) {
     movieInfoContainerEl.appendChild(overview);
 
   }
-};
-
-var getMovie = function (movie) {
-  var apiUrl =
-    "https://api.themoviedb.org/3/search/movie?query=" +
-    JSON.stringify(movie.replace(/\s/g, "-")) +
-    "&api_key=b2b7dc79b0696d3f9c1db98685b5b36f";
-  // "http://www.omdbapi.com/?s=" +
-  // JSON.stringify(movie.replace(/\s/g, "-")) +
-  // "&apikey=65b2c758";
-  //console.log(apiUrl);
-  // // make a request to the url
-  fetch(apiUrl)
-    .then(function (response) {
-      // request was successful
-      if (response.ok) {
-        response.json().then(function (data) {
-          //console.log(data);
-          displayMovies(data);
-        });
-      } else {
-        // error message if an invalid entery/movie is submitted
-        alert("Error: " + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      // catch set up incase Open Weather is down or internet is disconnected
-      alert("Unable to connect to The Movie Database");
-    });
-};
+}; */
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -78,11 +95,17 @@ var formSubmitHandler = function (event) {
   // get value from input element
   var movieTitle = movieInputEl.value.trim();
   if (movieTitle) {
-    getMovie(movieTitle);
+    getOmdb(movieTitle);
     movieInputEl.value = "";
   } else {
     alert("Please enter a movie title.");
   }
 };
+
+/* var displayWatchlist = function {
+  console.log()
+}; */
+
+
 
 movieFormEl.addEventListener("submit", formSubmitHandler);
