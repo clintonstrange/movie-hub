cb584032b1b0bc2731ca0f3616ce89da54809d65
BeatyPete <akira941@gmail.com>
2020-11-15 22:16:57 -0600

    made some changes to buttons to apply styling across class rather than by id

--- assets/js/script.js
+++ assets/js/script.js
@@ -243,13 +243,13 @@ var displayMovieSearch = function (movie) {
   movieInfoContainerEl.appendChild(btnContainerEl);
 
   var addToWatchListBtn = document.createElement("button");
-  addToWatchListBtn.classList = "addBtn m-1 column is-two-fifths";
+  addToWatchListBtn.classList = "addBtn m-1 column is-two-fifths watch-btn-styling";
   addToWatchListBtn.setAttribute("id", "add-to-watch-list-btn");
   addToWatchListBtn.textContent = "Add To Watch List";
   btnContainerEl.appendChild(addToWatchListBtn);
 
   var addToSeenListBtn = document.createElement("button");
-  addToSeenListBtn.classList = "addBtn m-1 column is-two-fifths";
+  addToSeenListBtn.classList = "addBtn m-1 column is-two-fifths seen-btn-styling";
   addToSeenListBtn.setAttribute("id", "add-to-seen-list-btn");
   addToSeenListBtn.textContent = "Add To Seen List";
   btnContainerEl.appendChild(addToSeenListBtn);
@@ -374,7 +374,12 @@ $("#random-btn").on("click", function() {
 $(".delete").on("click", function() {
   $("#random-modal").removeClass("is-active")
 });
-
+$("#random-watch-btn").on("click", function() {
+  console.log("add to watchlist") 
+});
+$("#random-seen-btn").on("click", function() {
+  console.log("add to seenlist")
+});
 
 movieFormEl.addEventListener("submit", formSubmitHandler);
 
